946e0511c8eee1e79f5bad027ef84c35808c40fe
Spartak <testwarfase@mail.ru>
2020-09-15 18:33:01 +0300

    third

--- lab1/var.js
+++ lab1/var.js
@@ -4,11 +4,15 @@ function isObj(tmp){
 }
 
 
-//var4
+//var 4
 function deCap(str){
     let  f = str[0]
     str = str.substr(1, str.length)
     return f.toLowerCase() + str
 }
 
-//
+
+//var 14
+function isInt(n){
+    return Number.isInteger(n);
+}
